8565447c75e435a608c71477add77fa84a2ae8b2
Muhammad Ali Khan <makaic18@gmail.com>
2026-04-03 17:11:42 +0500

    done

--- src/App.js
+++ src/App.js
@@ -192,7 +192,6 @@ function App() {
         <div style={{ display: 'flex', justifyContent: 'center', gap: '24px', marginTop: '20px' }}>
           {[
             { label: 'GitHub', url: ("https://github.com/aliikhann24") },
-            { label: 'LinkedIn', url: ("www.linkedin.com/in/muhammad-ali-khan-6363503aa")},
             { label: 'Portfolio', url: ("https://portfolio-murex-zeta-r1kpl6bzix.vercel.app/") },
           ].map(link => (
             <a key={link.label} href={link.url} target="_blank" rel="noreferrer"
